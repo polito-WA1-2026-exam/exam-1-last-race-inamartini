606b85aea2151ae23496a00b52c1af789a68ee33
Ina <inamar@stud.ntnu.no>
2026-05-30 15:23:16 +0200

    feat(database): set up database

---
+++ server/entities.js
@@ -0,0 +1,74 @@
+class User {
+    constructor(user_id, username, email, password_hashed) {
+        this.user_id = user_id;
+        this.username = username;
+        this.email = email;
+        this.password_hashed = password_hashed;
+    }
+}
+
+class Game {
+    constructor(game_id, user_id, start_station_id, destination_station_id, status, created_at, score = 20) {
+        this.game_id = game_id;
+        this.user_id = user_id;
+        this.start_station_id = start_station_id;
+        this.destination_station_id = destination_station_id;
+        this.score = score;
+        this.status = status;
+        this.created_at = created_at;
+    }
+}
+
+class GameAction {
+    constructor(game_action_id, game_id, step_number, from_station_id, to_station_id, event_id, remaining_coins) {
+        this.game_action_id = game_action_id;
+        this.game_id = game_id;
+        this.step_number = step_number;
+        this.from_station_id = from_station_id;
+        this.to_station_id = to_station_id;
+        this.event_id = event_id;
+        this.remaining_coins = remaining_coins;
+    }
+}
+
+class Event {
+    constructor(event_id, event_description, effect) {
+        this.event_id = event_id;
+        this.event_description = event_description;
+        this.effect = effect;
+    }
+}
+
+class Station {
+    constructor(station_id, station_name) {
+        this.station_id = station_id;
+        this.station_name = station_name;
+    }
+}
+
+class Segment {
+    constructor(segment_id, station_1_id, station_2_id) {
+        this.segment_id = segment_id;
+        this.station_1_id = station_1_id;
+        this.station_2_id = station_2_id;
+    }
+}
+
+class Line {
+    constructor(line_id, line_name, color, stations = []) {
+        this.line_id = line_id;
+        this.line_name = line_name;
+        this.color = color;
+        this.stations = stations;
+    }
+}
+
+export {
+    User,
+    Game,
+    GameAction,
+    Event,
+    Station,
+    Segment,
+    Line
+};
