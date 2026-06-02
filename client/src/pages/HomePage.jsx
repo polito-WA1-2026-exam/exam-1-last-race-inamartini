da4e5dc5b82b63939e058264becf2c6b4c0138e5
Ina <inamar@stud.ntnu.no>
2026-06-02 18:19:45 +0200

    fix(frontend): check user id

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ function HomePage({ user }) {
             <h1>Last Race</h1>
             <p>Race through the underground network to reach your destination before time runs out.</p>
             <div className="btn-container">
-                {user
+                {user?.id
                     ? <Link to="/game" className="btn btn-primary">Start a new game</Link>
                     : <Link to="/login" className="btn btn-primary">Login to play</Link>
                 }
